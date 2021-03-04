580a1aecbb956c8f37dce61f3103a82590ee0d87
Touch Sungkawichai <talay@10ninox.com>
2021-03-05 00:27:56 +0700

    add index link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Link, Button } from '@chakra-ui/react'
 
 
 const Page = () => (
-  <Box>
-    Introduction here
+  <Box p={5}>
+    Introduction page here
+    <Box>
+      <Link href="/timeline"><Button>timeline</Button></Link>
+      <Link href="/system/communism"><Button>communism</Button></Link>
+      <Link href="/system/liberalism"><Button>liberalism</Button></Link>
+      <Link href="/system/socialism"><Button>socialism</Button></Link>
+      <Link href="/system/capitalism"><Button>capitalism</Button></Link>
+    </Box>
   </Box>
 )
 
