e24a8845dbacc563bc5750a3afa810e90ab1b215
Touch Sungkawichai <talay@10ninox.com>
2021-03-21 21:56:16 +0700

    add socialist evnts

--- src/constants/timeline.ts
+++ src/constants/timeline.ts
@@ -8,34 +8,48 @@ export const events : IEvent[] = [
     content: 'French people took down the monarchy and established democratic houses.'
   },
   {
-    type: 'socialism',
+    type: 'communism',
     year: 1917,
     title: 'Russian Revolution',
     content: 'Lenin rose to power and rule the USSR, the first communist party to reach this level.'
   },
   {
-    type: 'socialism',
+    type: 'communism',
     year: 1948,
     title: 'Devision of Korea',
     content: 'Korea split upon the 38th parallel into North Korea and South Korea. North Korea, by Kim Il Sung, is regulated under the communism idea.'
   },
   {
-    type: 'socialism',
+    type: 'communism',
     year: 1949,
     title: "People's Republic of China",
-    content: "Mao Zedong founded People's Republic of China and became the leader of the communist state."
+    content: "Mao Zedong founded People's Republic of China and became the leader of the communists state."
+  },
+  {
+    type: 'socialism',
+    year: 1980,
+    displayYear: '1980s',
+    title: 'Socialist Parties',
+    content: 'Socialist parties are formed around the world in this era. This include socialist parties of USA, France, UK, Thailand, and other countries.'
+  },
+  {
+    type: 'socialism',
+    year: 1950,
+    displayYear: '19XX',
+    title: 'Social Democracy',
+    content: 'Socialism get applied with democracy as opposed to authoritarian. Socialists rebranded socialism as argued that socialism also works well in democratic society.'
   },
   {
     type: 'liberalism',
     year: 1863,
     title: "Abolish of Slavery, USA",
-    content: "Lincoln declared that all enslaved people should be freed. He ended all form of slavery in the United State of America."
+    content: "Lincoln declared that all enslaved people should be freed. Slavery in the United State of America was ended."
   },
   {
     type: 'capitalism',
     year: 1611, 
     title: 'Stock Market',
-    content: 'The first modern stock market originated in Amsterdam. The exchange contained just one company, The Dutch East India Company.'
+    content: 'The first modern stock market originated in Amsterdam. The exchange contained just one company, The Dutch East India Company. It later expanded.'
   },
   {
     type: 'capitalism',
@@ -48,7 +62,7 @@ export const events : IEvent[] = [
     type: 'liberalism',
     year: 2009,
     title: 'Genesis of Bitcoin',
-    content: 'Bitcoin - A decentralized protocol for peer-to-peer monetary system that requires no trust.'
+    content: 'Bitcoin - A decentralized protocol for peer-to-peer monetary system that requires no trust. It completely eliminated government intervention from the market.'
   },
   {
     type: 'liberalism',
